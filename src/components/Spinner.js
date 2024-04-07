f3c647db022a8dae6ba306575d0a5326e87fc372
Ansh Pandey <anshpandey8368@gmail.com>
2024-04-07 21:28:25 +0530

    Change in spinner position in center

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Spinner.css'
 
 const Spinner = () => {
-    return(
-        <div className='spinner'>
-
-        </div>
-    );
+  return(
+    <div className='spinner-container'>
+      <div className='spinner'></div>
+    </div>
+  );
 }
 
 export default Spinner;
